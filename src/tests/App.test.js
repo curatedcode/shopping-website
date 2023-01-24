06ec2fb5bed6d54cc93f48080f9750d88ba6ab4f
Zack F <zackaryfotheringham@hotmail.com>
2023-01-24 14:04:43 -0500

    add testing for rendering of api data

---
+++ src/tests/App.test.js
@@ -0,0 +1,14 @@
+import { render, screen, waitForElementToBeRemoved } from "@testing-library/react"
+import '@testing-library/jest-dom'
+import App from "../App"
+
+it('fetches and renders data',async() => {
+  render(<App />)
+  
+  await waitForElementToBeRemoved(screen.queryByText(/Loading.../i))
+
+  expect(screen.queryByText(/Fake Phone/i)).toBeVisible()
+  expect(screen.queryByText(/189/i)).toBeVisible()
+  expect(screen.queryByText(/4.69/i)).toBeVisible()
+  expect(screen.queryByAltText(/Fake Phone/i)).toBeVisible()
+})
