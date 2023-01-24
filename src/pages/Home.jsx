c914fd0ff1cc1eefbf17d4bc49ba9ce405c81eb2
Zack F <zackaryfotheringham@hotmail.com>
2023-01-24 14:03:58 -0500

    render all products on home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,19 @@
+import ItemCard from "../components/ItemCard"
+import { v4 as uuidv4 } from 'uuid'
+import { useQuery } from "@tanstack/react-query"
+import getAllProducts from "../api/allProducts"
 
 function Home(){
+  const { status, error, data } = useQuery({
+    queryKey: ['productData'],
+    queryFn: getAllProducts,
+  })
+  if(status === 'loading') return <h1>Loading...</h1>
+  if(status === 'error') return <h1>{JSON.stringify(error)}</h1>
   return(
-    <>
-      <span>Home Page</span>
-    </>
+    <div className="">
+      {data.products.map(item => <ItemCard data={item} key={uuidv4()}/>)}
+    </div>
   )
 }
 
