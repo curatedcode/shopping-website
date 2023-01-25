d02a0f317e32a16592371a06871661de8c31dd97
Zack F <zackaryfotheringham@hotmail.com>
2023-01-24 22:59:31 -0500

    add await sections

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -3,7 +3,6 @@ import App from "../App"
 
 it('fetches and renders data',async() => {
   render(<App />)
-  
   await waitForElementToBeRemoved(screen.queryByText(/Loading.../i))
 
   expect(screen.queryByText(/Fake Phone/i)).toBeVisible()
@@ -16,11 +15,12 @@ it('clicking on product brings you to that products page', async () => {
   render(<App />)
 
   fireEvent.click(screen.queryByText(/Fake Phone/i))
-
+  await waitForElementToBeRemoved(screen.queryByText(/Loading.../i))
   const allPictures = screen.getAllByRole('img', {name: /Fake Phone/i})
   expect(allPictures.length).toBe(5)
   allPictures.forEach(img => expect(img).toBeVisible())
 
   const allPrices = screen.getAllByText(/189/i)
   expect(allPrices.length).toBe(2)
+  expect(screen.queryByText(/A phone that has never been made before!/i)).toBeVisible()
 })
