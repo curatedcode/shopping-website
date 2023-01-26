f42133200b6a1e827014b81439f7dcf0f75789f1
Zack F <zackaryfotheringham@hotmail.com>
2023-01-26 01:28:31 -0500

    add pictures for hero section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,8 @@ import ItemCard from "../components/ItemCard"
 import { v4 as uuidv4 } from 'uuid'
 import { useQuery } from "@tanstack/react-query"
 import getAllProducts from "../api/allProducts"
+import HeroMobile from "../img/HeroMobile"
+import HeroDesktop from "../img/HeroDesktop"
 
 function Home(){
   const { status, error, data } = useQuery({
@@ -11,8 +13,15 @@ function Home(){
   if(status === 'loading') return <h1>Loading...</h1>
   if(status === 'error') return <h1>{JSON.stringify(error)}</h1>
   return(
-    <div className="">
-      {data.products.map(item => <ItemCard data={item} key={uuidv4()}/>)}
+    <div>
+      <div>
+        <div className="invisible w-0 md:visible md:w-auto"><HeroDesktop /></div>
+        <div className="md:invisible md:w-0"><HeroMobile/></div>
+      </div>
+      <div className="text-white bg-red-700 text-center py-1 text-lg">Trending Products</div>
+      <div className="grid justify-items-center my-6 gap-y-6">
+        {data.products.map(item => <ItemCard data={item} key={uuidv4()}/>)}
+      </div>
     </div>
   )
 }
