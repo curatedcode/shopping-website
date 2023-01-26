72e48ab88227fcea7884fbefdb2e9bf45d133901
Zack F <zackaryfotheringham@hotmail.com>
2023-01-26 01:27:58 -0500

    add styling and icons

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,16 +1,25 @@
 import { AiOutlineShoppingCart } from "react-icons/ai"
+import { RiBearSmileLine } from "react-icons/ri"
+import { RxHamburgerMenu } from "react-icons/rx"
 import { Outlet, Link } from "react-router-dom"
 
 function NavBar(){
   return(
     <>
-    <nav>
-      <ul>
-        <li className="title-font font-bold">
-          <Link to="/">Shop-R-Us</Link>
-        </li>
+    <nav className="px-5 py-3">
+      <ul className="grid grid-cols-3 items-center text-2xl">
         <li>
-          <Link to="/cart">{<AiOutlineShoppingCart />}Cart</Link>
+          <RxHamburgerMenu />
+        </li>
+        <li className="title-font place-self-center">
+          <Link to="/">
+            <RiBearSmileLine className="fill-red-700 text-3xl" />
+          </Link>
+        </li>
+        <li className="place-self-end">
+          <Link className="" to="/cart">
+            <AiOutlineShoppingCart />
+          </Link>
         </li>
       </ul>
     </nav>
