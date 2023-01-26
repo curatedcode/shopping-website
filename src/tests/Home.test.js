1d0adf1f27130a47f9d0a6b718f15a1c0502b63f
Zack F <zackaryfotheringham@hotmail.com>
2023-01-26 01:25:52 -0500

    add test for initial render of data

--- src/tests/Home.test.js
+++ src/tests/Home.test.js
@@ -4,6 +4,27 @@ import { MemoryRouter, Route, Routes } from "react-router-dom";
 import Home from "../pages/Home";
 import Product from "../pages/Product";
 
+it('fetches and renders api data',async() => {
+  const queryClient = new QueryClient()
+  const CustomHome = () =>{
+    return(
+      <MemoryRouter initialEntries={['/']}>
+        <QueryClientProvider client={queryClient}>
+            <Routes>
+              <Route path="/" element={<Home />} />
+            </Routes>
+        </QueryClientProvider>
+      </MemoryRouter>
+    )
+  }
+  render(<CustomHome />)
+  await waitForElementToBeRemoved(screen.queryByText(/Loading.../i))
+
+  expect(screen.getByText(/Fake Phone/i)).toBeVisible()
+  expect(screen.getByText(/189/i)).toBeVisible()
+  expect(screen.getByAltText(/Fake Phone/i)).toBeVisible()
+})
+
 it('clicking on product brings you to that products page', async () => {
   const queryClient = new QueryClient()
   const CustomHome = () =>{
@@ -29,5 +50,8 @@ it('clicking on product brings you to that products page', async () => {
 
   const allPrices = screen.getAllByText(/189/i)
   expect(allPrices.length).toBe(2)
-  expect(screen.queryByText(/A phone that has never been made before!/i)).toBeVisible()
+  expect(screen.getByText(/A phone that has never been made before!/i)).toBeVisible()
 })
+
+//test for cart button bringing you to cart page
+//test for nav button brining up nav page
