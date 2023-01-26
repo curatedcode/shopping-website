aad97e3c911f0f440328cab125e697ac29413c0b
Zack F <zackaryfotheringham@hotmail.com>
2023-01-26 07:07:22 -0500

    change function to a import statement

--- src/components/ItemCard.jsx
+++ src/components/ItemCard.jsx
@@ -1,33 +1,18 @@
 import { Link } from "react-router-dom"
-import { AiOutlineStar, AiFillStar } from "react-icons/ai"
 import { useEffect, useState } from "react"
-import { v4 as uuidv4 } from 'uuid'
+import colorReviewStars from "./colorReviewStars"
 
 function ItemCard(props){
-  const [stars, setStars] = useState()
-  function colorReviewStars(rating){
-    const numToColor = Math.floor(rating)
-    let allStarsArray = []
-    for(let i=0; i<numToColor; i++){
-      allStarsArray.push(<AiFillStar key={uuidv4()} className="fill-orange-500" />)
-    }
-    if(allStarsArray.length !== 5){
-      const leftToFill = 5 - allStarsArray.length
-      for(let i=0; i<leftToFill; i++){
-        allStarsArray.push(<AiOutlineStar key={uuidv4()} className="fill-gray-600" />)
-      }
-    }
-    setStars(allStarsArray)
-  }
+  const [stars, setStars] = useState([])
   useEffect(()=>{
-    colorReviewStars(props.data.rating)
+    setStars(colorReviewStars(props.data.rating))
   },[props.data.rating])
   return(
     <Link to={`/product/${props.data.id}`} className="grid grid-cols-2 gap-x-3 px-4">
-      <img className="aspect-square rounded-lg" src={props.data.thumbnail} alt={props.data.title}></img>
+      <img className="rounded-lg" src={props.data.thumbnail} alt={props.data.title}></img>
       <div className="text-lg">
         <span className="">{props.data.title}</span>
-        <div data-testid="stars" className="flex items-center">{stars}</div>
+        <div className="flex items-center">{stars}</div>
         <div className="flex items-center font-semibold">
           <span className="text-sm">$</span>
           <span>{props.data.price}</span>
