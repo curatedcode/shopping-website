345474fa02fab965b49d7a3bf7f112dd80189b2a
Zack F <zackaryfotheringham@hotmail.com>
2023-01-26 07:07:40 -0500

    add footer

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { Routes, Route, BrowserRouter } from "react-router-dom";
+import Footer from "./components/Footer";
 import NavBar from "./components/NavBar";
 import Cart from "./pages/Cart";
 import Home from "./pages/Home";
 import NotFound from "./pages/NotFound";
 import Product from "./pages/Product";
 
-if (process.env.NODE_ENV === 'development') {
-  const { worker } = require('./mocks/browser')
-  worker.start()
-}
+// if (process.env.NODE_ENV === 'development') {
+//   const { worker } = require('./mocks/browser')
+//   worker.start()
+// }
 
 const queryClient = new QueryClient()
 
@@ -20,16 +21,17 @@ function App() {
       <QueryClientProvider client={queryClient}>
         <Routes>
           <Route element={<NavBar />}>
-          <Route path="/" element={<Home />} />
-          <Route path="/product/:id" element={<Product />} />
-          <Route path="/cart" element={<Cart />} />
+            <Route element={<Footer />}>
+              <Route path="/" element={<Home />} />
+              <Route path="/product/:id" element={<Product />} />
+              <Route path="/cart" element={<Cart />} />
+            </Route>          
           </Route>
           <Route path="*" element={<NotFound />}/>
         </Routes>    
         <ReactQueryDevtools />
       </QueryClientProvider>
     </BrowserRouter>
-
   );
 }
 
---
+++ src/components/Footer.jsx
@@ -0,0 +1,25 @@
+import { Link, Outlet } from "react-router-dom";
+
+
+function Footer(){
+  return(
+    <>
+    <Outlet />
+    <footer className="bg-red-700 sticky w-full">
+      <ul className="flex justify-evenly py-4 text-sm text-white">
+        <Link to='/contact'>
+          <li>Contact Us</li>
+        </Link>
+        <Link to='/privacy-policy'>
+          <li>Privacy Policy</li>
+        </Link>
+        <Link to='/about'>
+          <li>About Us</li>
+        </Link>
+      </ul>
+    </footer>
+    </>
+  )
+}
+
+export default Footer
