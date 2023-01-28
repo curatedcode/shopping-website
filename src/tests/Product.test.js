012c0dd9f6522b03efa069551c82985eb5a3b17f
Zack F <zackaryfotheringham@hotmail.com>
2023-01-28 02:54:18 -0500

    add test for rendering

--- src/tests/Product.test.js
+++ src/tests/Product.test.js
@@ -3,6 +3,34 @@ import { fireEvent, render, screen, waitForElementToBeRemoved } from "@testing-l
 import { MemoryRouter, Route, Routes } from "react-router-dom";
 import Product from "../pages/Product";
 
+describe('product component',()=>{
+  
+  it('should render all data properly',async()=>{
+    const queryClient = new QueryClient()
+    const CustomProduct = () =>{
+      return(
+        <MemoryRouter initialEntries={['/products/1']}>
+          <QueryClientProvider client={queryClient}>
+            <Routes>
+              <Route path="/products/:id" element={<Product />} />
+            </Routes>
+          </QueryClientProvider>
+         </MemoryRouter>
+      )
+    }
+    render(<CustomProduct />)
+    await waitForElementToBeRemoved(screen.queryByText(/Loading.../i))
+
+    expect(screen.getAllByRole('img', { name: 'Fake Phone' }).length).toBe(5)
+    expect(screen.getByText(/in stock/i)).toBeVisible()
+    expect(screen.getByText(/friday, january 27/i)).toBeVisible()
+    expect(screen.getByTestId(/quantity/i).value).toEqual('1')
+    expect(screen.getAllByText(/Fake Phone/i).length).toBe(2)
+    expect(screen.getByText(/Faker/i)).toBeVisible()
+    expect(screen.getByText(/A phone that has never been made before!/i)).toBeVisible()
+  })
+})
+
 describe('order quantity input',()=>{
 
   it('should be 1 on initial page load',async()=>{
