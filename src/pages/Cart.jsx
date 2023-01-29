66b7b4e171992ba892865deecc547356f283c545
Zack F <zackaryfotheringham@hotmail.com>
2023-01-28 20:20:04 -0500

    add orderTotal to cart

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -5,7 +5,7 @@ import { v4 as uuidv4 } from "uuid"
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query"
 import handleQuantityChange from "../api/handleQuantityChange"
 import { Link } from "react-router-dom"
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import { removeItem, updateCartItemQuantity } from "../api/cartData"
 
 function Cart(){
@@ -26,32 +26,49 @@ function Cart(){
   function handleChange(e, itemId, currItemQuantity){
     const newQuantity = handleQuantityChange(e, currItemQuantity)
     editQuantity.mutate({id: itemId, quantity: newQuantity})
+    updateCartTotal()
   }
+  function storeOrderTotal(){
+    localStorage.setItem('orderTotal', JSON.stringify(allItemsTotal))
+  }
+  function updateCartTotal(){
+    let totalPrice = 0
+    if(status === 'success') data.map(item => totalPrice += item.price)
+    setAllItemsTotal(totalPrice)
+  }
+  useEffect(()=>{
+    updateCartTotal()
+  })
 
   if(status === 'loading') return <h1>Loading Your Cart...</h1>
   if(status === 'error') return <h1>{JSON.parse(error)}</h1>
   return(
     <>
-      <div className="text-gray-200 bg-red-700 py-3 text-sm flex justify-center gap-2 items-center font-semibold mb-4"><GoLocation className="text-xl fill-gray-200" /> Select a location to see product availability</div>
-      <div>
-        <span>Subtotal</span>
-        <span>$</span>
-        <span>{allItemsTotal}</span>
-      </div>
       <div className="h-screen">
         {data?.length > 0 ? (
-          data.map(item => 
-          <div key={uuidv4()}>
-            <CartItemCard data={item}/>
-            <div className="flex gap-2">
-              <BiTrash className={`${ Number(item.stock) > 1 ? 'hidden':''}`} onClick={()=>deleteItem.mutate(item.id)}  />
-              <button className={`${ Number(item.stock) > 1 ? '':'hidden'}`} onClick={(e)=>handleChange(e, item.id, item.quantity)} name="decrease-quantity" type="button">-</button>
-              <span>{item.quantity}</span>
-              <button onClick={(e)=>handleChange(e, item.id, item.quantity)} name="increase-quantity" type="button">+</button>
-              <button onClick={()=>deleteItem.mutate(item.id)} type={'button'}>Delete</button>
+          <>
+          <Link to="/checkout">
+            <button onClick={storeOrderTotal} className="bg-red-700 text-gray-200 py-2 px-6 rounded-full font-semibold w-full" type="button" name="checkout">Proceed to checkout</button>
+          </Link>
+          {data.map(item => 
+            <div key={uuidv4()}>
+              <CartItemCard data={item}/>
+              <div className="flex gap-2">
+                <BiTrash className={`${ Number(item.stock) > 1 ? 'hidden':''}`} onClick={()=>deleteItem.mutate(item.id)}  />
+                <button className={`${ Number(item.stock) > 1 ? '':'hidden'}`} onClick={(e)=>handleChange(e, item.id, item.quantity)} name="decrease-quantity" type="button">-</button>
+                <span>{item.quantity}</span>
+                <button onClick={(e)=>handleChange(e, item.id, item.quantity)} name="increase-quantity" type="button">+</button>
+                <button onClick={()=>deleteItem.mutate(item.id)} type={'button'}>Delete</button>
+              </div>
             </div>
+          )}
+          <div>
+            <span>Subtotal</span>
+            <span>$</span>
+            <span>{allItemsTotal}</span>
           </div>
-        )) : (
+          </>
+        ) : (
           <div>
             <div>No Items In Your Cart.</div>
             <Link to="/">
